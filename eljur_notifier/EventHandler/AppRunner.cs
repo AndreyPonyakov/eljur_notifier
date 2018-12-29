4fb4b91a1d40fcac9a603f8ddffa09784a2baac4
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2018-12-29 11:14:48 +0300

    AppRunner was added

---
+++ eljur_notifier/EventHandler/AppRunner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using eljur_notifier.FirebirdNS;
+using eljur_notifier.MsDbNS;
+using eljur_notifier.AppCommon;
+
+namespace eljur_notifier.EventHandlerNS
+{
+    class AppRunner
+    {
+        internal protected Message message { get; set; }
+        internal protected Config config { get; set; }
+        internal protected MsDb msDb { get; set; }
+        internal protected Firebird firebird { get; set; }
+        internal protected MsDbChecker msDbChecker { get; set; }
+        internal protected EventHandlerEljur eventHandlerEljur { get; set; }
+        internal protected CancellationTokenSource cancellationTokenSource { get; set; }
+
+        public AppRunner()
+        {
+            this.message = new Message();
+            this.config = new Config();
+            this.firebird = new Firebird(config.ConStrFbDB);
+            this.msDb = new MsDb(config.ConStrMsDB);
+            this.msDbChecker = new MsDbChecker(config, msDb, firebird);
+            this.eventHandlerEljur = new EventHandlerEljur(config, msDb, firebird, msDbChecker);
+            this.cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void Run(string[] args)
+        {
+            var GetDataFb = eventHandlerEljur.GetDataFb(cancellationTokenSource.Token);
+
+            var SendNotifyParents = eventHandlerEljur.SendNotifyParents(cancellationTokenSource.Token);
+
+            var CatchEventFirstPass = eventHandlerEljur.CatchEventFirstPass(cancellationTokenSource.Token);
+
+            var CatchEventLastPass = eventHandlerEljur.CatchEventLastPass(cancellationTokenSource.Token);
+
+
+
+            var CheckMsDb = eventHandlerEljur.ChecTimekMsDb(cancellationTokenSource.Token, new Action(delegate
+            {
+                cancellationTokenSource.Cancel();
+                Task.WaitAll(GetDataFb, SendNotifyParents, CatchEventFirstPass, CatchEventLastPass);
+                Task.Delay(120000);
+                //restart                              
+                Run(args);
+
+            }));
+
+            while (true) { }
+        }
+
+    }
+}
--- eljur_notifier/Program.cs
+++ eljur_notifier/Program.cs
@@ -1,75 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Data;
-using System.Data.SqlClient;
-using eljur_notifier;
-using eljur_notifier.StaffModel;
-using eljur_notifier.AppCommon;
-using eljur_notifier.FirebirdNS;
-using eljur_notifier.MsDbNS;
 using eljur_notifier.EventHandlerNS;
-using NLog;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+
 
 
 namespace eljur_notifier
 {
     class Program
     {
+        internal protected AppRunner appRunner { get; set; }
         public static List<string> MainMethodArgs = new List<string>();
         static void Main(string[] args)
         {
             MainMethodArgs = args.ToList();
-            Run(MainMethodArgs.ToArray());
-        }
-
-        public static void Run(string[] args)
-        {
-            //Console.WriteLine(DateTime.Today.ToShortDateString());
-            //Console.WriteLine(DateTime.Today.Month );
-            //Console.WriteLine(DateTime.Today.Day );
-            //if ((Convert.ToInt32(DateTime.Today.Day) == 28) && (Convert.ToInt32(DateTime.Today.Month) == 12))
-            //{
-            //    Console.WriteLine("True");
-            //}
-
-            //Console.ReadKey();
-
-            Message message = new Message();
-            Config Config = new Config();         
-            Firebird Firebird = new Firebird(Config.ConStrFbDB);        
-            MsDb MsDb = new MsDb(Config.ConStrMsDB);
-            MsDbChecker MsDbChecker = new MsDbChecker(Config, MsDb, Firebird);
-            EventHandlerEljur EventHandler = new EventHandlerEljur(Config, MsDb, Firebird, MsDbChecker);
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-            var GetDataFb = EventHandler.GetDataFb(cancellationTokenSource.Token);
-
-            var SendNotifyParents = EventHandler.SendNotifyParents(cancellationTokenSource.Token);
-
-            var CatchEventFirstPass = EventHandler.CatchEventFirstPass(cancellationTokenSource.Token);
-
-            var CatchEventLastPass = EventHandler.CatchEventLastPass(cancellationTokenSource.Token);
-
-          
-
-            var CheckMsDb = EventHandler.ChecTimekMsDb(cancellationTokenSource.Token, new Action(delegate
-            {
-                cancellationTokenSource.Cancel();
-                Task.WaitAll(GetDataFb, SendNotifyParents, CatchEventFirstPass, CatchEventLastPass);
-                Task.Delay(120000);
-                //restart                              
-                Run(MainMethodArgs.ToArray());
-
-            }));
-       
-            while (true) { }
-        }
+            AppRunner appRunner = new AppRunner();
+            appRunner.Run(MainMethodArgs.ToArray());
+        }     
    
     }
 }
